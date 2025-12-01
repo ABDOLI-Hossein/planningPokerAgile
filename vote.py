6d7c1816100b1d89236fb4a38a945121d2ab2270
Hossein ABDOLI <72072994+ABDOLI-Hossein@users.noreply.github.com>
2025-12-01 14:59:09 +0100

    Update vote.py

--- vote.py
+++ vote.py
@@ -1,31 +1,33 @@
-##
-# @file vote.py
-# @brief Contient la classe Vote, représentant le vote d'un joueur pour une story.
-# @version 1.0
+"""
+@file vote.py
+@brief Defines the Vote class.
+"""
+
+from user import User
+from card import Card
 
 class Vote:
-    ##
-    # @brief Constructeur d'un vote.
-    # @param user Joueur qui vote (instance de User).
-    # @param card Carte choisie par le joueur (instance de Card).
-    def __init__(self, user, card):
+    """
+    @class Vote
+    @brief Represents a vote made by a user.
+    """
+
+    def __init__(self, user: User, card: Card):
+        """
+        @param user The voting user
+        @param card The chosen card
+        """
         self.user = user
         self.card = card
 
-    ##
-    # @brief Retourne le joueur ayant voté.
-    # @return Instance de User.
     def get_user(self):
+        """
+        @return User object
+        """
         return self.user
 
-    ##
-    # @brief Retourne la carte choisie.
-    # @return Instance de Card.
     def get_card(self):
+        """
+        @return Card object
+        """
         return self.card
-
-    ##
-    # @brief Représentation textuelle du vote.
-    # @return Chaîne décrivant le vote.
-    def __str__(self):
-        return f"Vote(user={self.user.get_name()}, card={self.card.get_value()})"
