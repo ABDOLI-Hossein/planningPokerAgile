7f4cbe3dd37bb2c2050b90e6b37c9e540e6f7d59
ABDOLI-Hossein <ABDOLI-Hossein@users.noreply.github.com>
2025-12-01 13:42:58 +0000

    deploy: e7525d64315727fbd5379f54092acefffb08912a

--- search/all_0.js
+++ search/all_0.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['_5finit_5f_0',['_init_',['../dd/d55/classcard_1_1Card.html#a03a25b06a9c20356eb5c26d48e3e3fdf',1,'card.Card._init_()'],['../d0/dad/classdeck_1_1Deck.html#acc143783437f3195fee1bd1b153aeaf8',1,'deck.Deck._init_()'],['../dc/dd0/classsession_1_1Session.html#ab640f501f07835fb098773dbb9264868',1,'session.Session._init_()'],['../d4/d24/classstory_1_1Story.html#a6ca7fed36769e72764d824f6f60393e7',1,'story.Story._init_()'],['../dc/dbf/classuser_1_1User.html#ae104e2d34d08c4e3d3868c6396771ff7',1,'user.User._init_()'],['../db/d53/classvote_1_1Vote.html#aa1fac581041236831d274ecfae626b37',1,'vote.Vote._init_()']]]
+  ['_5f_5finit_5f_5f_0',['__init__',['../dc/dbf/classuser_1_1User.html#a3bda81c3c16a25e1e85864efecf1b7fb',1,'user::User']]],
+  ['_5f_5fstr_5f_5f_1',['__str__',['../dc/dbf/classuser_1_1User.html#a6c1b86237af155ccd5396102ecb41ab8',1,'user::User']]],
+  ['_5finit_5f_2',['_init_',['../dd/d55/classcard_1_1Card.html#a03a25b06a9c20356eb5c26d48e3e3fdf',1,'card.Card._init_()'],['../d0/dad/classdeck_1_1Deck.html#acc143783437f3195fee1bd1b153aeaf8',1,'deck.Deck._init_()'],['../dc/dd0/classsession_1_1Session.html#ab640f501f07835fb098773dbb9264868',1,'session.Session._init_()'],['../d4/d24/classstory_1_1Story.html#a6ca7fed36769e72764d824f6f60393e7',1,'story.Story._init_()'],['../db/d53/classvote_1_1Vote.html#aa1fac581041236831d274ecfae626b37',1,'vote.Vote._init_()']]]
 ];
--- search/functions_5.js
+++ search/functions_5.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['get_5fname_0',['get_name',['../dc/dbf/classuser_1_1User.html#ad7d281ef7e67140eb8c44f50d87edca0',1,'user::User']]]
+  ['get_5fid_0',['get_id',['../dc/dbf/classuser_1_1User.html#ad6ef4fa5bea74c8768f74c4857456f2f',1,'user::User']]],
+  ['get_5fname_1',['get_name',['../dc/dbf/classuser_1_1User.html#ad7d281ef7e67140eb8c44f50d87edca0',1,'user::User']]],
+  ['get_5frole_2',['get_role',['../dc/dbf/classuser_1_1User.html#a7825285767503b132ac758a41d0a7f08',1,'user::User']]]
 ];
