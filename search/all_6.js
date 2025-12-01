96bcf0339da3d23d67ae47857d7252cf112b2a41
ABDOLI-Hossein <ABDOLI-Hossein@users.noreply.github.com>
2025-12-01 13:43:27 +0000

    deploy: 5093ee8728b63addd1c3bd6eccb45755d0bf669f

--- search/all_6.js
+++ search/all_6.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['get_5fid_0',['get_id',['../dc/dbf/classuser_1_1User.html#ad6ef4fa5bea74c8768f74c4857456f2f',1,'user::User']]],
   ['get_5fname_1',['get_name',['../dc/dbf/classuser_1_1User.html#ad7d281ef7e67140eb8c44f50d87edca0',1,'user::User']]],
-  ['get_5frole_2',['get_role',['../dc/dbf/classuser_1_1User.html#a7825285767503b132ac758a41d0a7f08',1,'user::User']]]
+  ['get_5frole_2',['get_role',['../dc/dbf/classuser_1_1User.html#a7825285767503b132ac758a41d0a7f08',1,'user::User']]],
+  ['get_5fvalue_3',['get_value',['../dd/d55/classcard_1_1Card.html#a019a12240977575ecc468f2bc39cdaf8',1,'card::Card']]]
 ];
