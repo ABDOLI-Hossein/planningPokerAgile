a83bb0f3cd49e80970704b5450c50a8afe9f4fdf
ABDOLI-Hossein <ABDOLI-Hossein@users.noreply.github.com>
2025-12-01 13:44:50 +0000

    deploy: 44d7786e63cbe02567081ccb620624d257f5da7a

--- search/all_2.js
+++ search/all_2.js
@@ -4,5 +4,6 @@ var searchData=
   ['card_1',['card',['../dd/d55/classcard_1_1Card.html',1,'card.Card'],['../d4/dd2/namespacecard.html',1,'card'],['../db/d53/classvote_1_1Vote.html#a1ba164fa904a1faed51a0d935d7d2dad',1,'vote.Vote.card']]],
   ['card_2epy_2',['card.py',['../de/db6/card_8py.html',1,'']]],
   ['cards_3',['cards',['../d0/dad/classdeck_1_1Deck.html#aa56f510ed0ff2df43d8fc241f7e6d827',1,'deck::Deck']]],
-  ['current_5fstory_4',['current_story',['../dc/dd0/classsession_1_1Session.html#a01213211c249e56b555bb8cfc2c3a366',1,'session::Session']]]
+  ['cast_5fvote_4',['cast_vote',['../dc/dd0/classsession_1_1Session.html#a457f7c110677e11ef0996a1e1aac81ce',1,'session::Session']]],
+  ['current_5fstory_5',['current_story',['../dc/dd0/classsession_1_1Session.html#a01213211c249e56b555bb8cfc2c3a366',1,'session::Session']]]
 ];
