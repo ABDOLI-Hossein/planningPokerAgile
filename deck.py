262f460bb77c2e3db1f3db09346485f897eecb73
Hossein ABDOLI <72072994+ABDOLI-Hossein@users.noreply.github.com>
2025-12-01 14:58:51 +0100

    Update deck.py

--- deck.py
+++ deck.py
@@ -1,39 +1,26 @@
-##
-# @file deck.py
-# @brief Gère le paquet de cartes utilisées pour voter dans Planning Poker.
-# @version 1.0
+"""
+@file deck.py
+@brief Defines the deck of Planning Poker cards.
+"""
 
 from card import Card
 
 class Deck:
-    ##
-    # @brief Constructeur du paquet de cartes.
-    def __init__(self, cards=None):
-        self.cards = cards if cards is not None else []
+    """
+    @class Deck
+    @brief The complete set of Planning Poker cards.
+    """
 
-    ##
-    # @brief Crée un paquet standard (Fibonacci-like) avec quelques symboles.
-    # @return Un objet Deck contenant les cartes standards.
-    @staticmethod
-    def default_deck():
-        default_values = [1, 2, 3, 5, 8, 13, 21, "?", "☕"]
-        return Deck([Card(v) for v in default_values])
+    def __init__(self):
+        """
+        @brief Initializes all possible Planning Poker cards.
+        """
+        values = ["1", "2", "3", "5", "8", "13", "21", "?"]
+        self.cards = [Card(v) for v in values]
 
-    ##
-    # @brief Retourne la liste des cartes.
-    # @return Liste d'objets Card.
     def get_cards(self):
+        """
+        @brief Returns all cards.
+        @return list of Card
+        """
         return self.cards
-
-    ##
-    # @brief Ajoute une carte au paquet.
-    # @param card Objet Card à ajouter.
-    def add_card(self, card):
-        self.cards.append(card)
-
-    ##
-    # @brief Représentation textuelle du paquet.
-    # @return Chaîne listant les cartes.
-    def __str__(self):
-        vals = ', '.join(str(c.get_value()) for c in self.cards)
-        return f"Deck([{vals}])"
